ea70eb846ce6a586f15513089119a100ea080686
klub-ice <114689677+klub-ice@users.noreply.github.com>
2025-09-03 21:45:31 -0500

    changed hello to bye

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-//main initilzation to print out hello world
+// main initilzation to print out bye world
 int main() {
-    printf("%s\n", "hello world");
+    printf("%s\n", "bye world");
     return 0;
 }
